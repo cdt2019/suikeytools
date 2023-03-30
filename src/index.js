f20c54272e80f58d7796b3f15058437ed0d8e5a7
chenxw <chenxw@163.com>
2023-03-30 18:26:37 +0800

    change batch gen key

--- src/index.js
+++ src/index.js
@@ -1,23 +1,128 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Genkey = void 0;
 const sui_js_1 = require("@mysten/sui.js");
 const bip39_1 = require("./bip39");
 const sui_js_2 = require("@mysten/sui.js");
 const utils_1 = require("@noble/hashes/utils");
-class Genkey {
-    static gen() {
+const ts_command_line_args_1 = require("ts-command-line-args");
+const os_1 = __importDefault(require("os"));
+const exceljs_1 = __importDefault(require("exceljs"));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const date_fns_1 = require("date-fns");
+class GenSuikey {
+    genKey() {
         const mnemonic = (0, bip39_1.generateMnemonic)();
         const ed25519Keypair = sui_js_1.Ed25519Keypair.deriveKeypair(mnemonic);
         const publicKey = ed25519Keypair.getPublicKey();
         const address = publicKey.toSuiAddress();
         const privateKeyStr = `0x${(0, utils_1.bytesToHex)((0, sui_js_2.fromB64)(ed25519Keypair.export().privateKey).slice(0, 32))}`;
         const publicKeyStr = `0x${(0, utils_1.bytesToHex)(publicKey.toBytes())}`;
-        console.log(mnemonic);
-        console.log(publicKeyStr);
-        console.log(privateKeyStr);
-        console.log(address);
+        return {
+            mnemonic: mnemonic,
+            address: address,
+            privateKey: privateKeyStr,
+            publicKey: publicKeyStr,
+        };
     }
 }
-exports.Genkey = Genkey;
-Genkey.gen();
+//parse command line argruments
+const args = (0, ts_command_line_args_1.parse)({
+    filepath: { type: String, alias: 'p', description: 'save file path', optional: true },
+    filename: { type: String, alias: 'f', description: 'save file name', optional: true },
+    num: { type: Number, alias: 'n', description: 'number of generate key', optional: true },
+});
+let { filepath, filename, num } = {
+    filepath: args.filepath || os_1.default.homedir(),
+    filename: args.filename || "sui_key.xlsx",
+    num: args.num || 20
+};
+//gen key
+let keyArr = [];
+console.log('%s %s %s %s', 'address', 'mnemonic', 'privateKey', 'publicKey');
+const genSuiKey = new GenSuikey();
+for (let idx = 0; idx < num; idx++) {
+    const keyInfo = genSuiKey.genKey();
+    console.log('%s "%s" %s %s', keyInfo.address, keyInfo.mnemonic, keyInfo.privateKey, keyInfo.publicKey);
+    keyArr.push(keyInfo);
+}
+let workbook = new exceljs_1.default.Workbook();
+let worksheet = workbook.addWorksheet();
+worksheet.columns = [
+    { header: "MNEMONIC", key: 'mnemonic', width: 90 },
+    { header: "ADDRESS", key: 'address', width: 45 },
+    { header: "PRIVATE KEY", key: 'privateKey', width: 70 },
+    { header: "PUBLIC KEY", key: 'publicKey', width: 70 },
+];
+// header row set bold and center
+let headerRow = worksheet.getRow(1);
+headerRow.eachCell(cell => {
+    cell.style = {
+        font: {
+            bold: true,
+        },
+        alignment: {
+            horizontal: 'center',
+        },
+    };
+});
+//add data
+worksheet.addRows(keyArr);
+//.xlsx
+if (!filename.endsWith('.xlsx')) {
+    filename += '.xlsx';
+}
+//write file
+let writeFilePath = path.join(filepath, filename);
+//exist file save new other file
+if (fs.existsSync(writeFilePath)) {
+    const filenameOption = path.parse(filename);
+    filename = filenameOption.name + "_" + (0, date_fns_1.format)(new Date(), 'yyyyMMddHHmmss') + filenameOption.ext;
+    writeFilePath = path.join(filepath, filename);
+}
+//write file
+console.log("key file save path: ", writeFilePath);
+writeKeyFile(workbook, writeFilePath);
+function writeKeyFile(workbook, writeFilePath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield workbook.xlsx.writeFile(writeFilePath);
+    });
+}
+console.log("save key file sucessfully!");
+// node .\src\index.js --filepath=C:\Users\cxw\Desktop\ --filename=sui-key.xlsx --num=20
+// node .\src\index.js --p=C:\Users\cxw\Desktop\ --f=sui-key --n=20
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,28 @@ import { generateMnemonic } from "./bip39";
 import { fromB64 } from '@mysten/sui.js';
 import { bytesToHex } from '@noble/hashes/utils';
 
-export class Genkey {
-    static gen() {
+import { parse } from 'ts-command-line-args';
+import os from 'os'
+import Excel from 'exceljs'
+import * as fs from 'fs';
+import * as path from 'path';
+import {format} from 'date-fns';
+
+interface GenKeyArguments {
+    filepath?: string,
+    filename?: string,
+    num?: number,
+}
+
+interface KeyInfo {
+    mnemonic: string,
+    address: string,
+    privateKey: string,
+    publicKey: string,
+}
+
+class GenSuikey {
+    genKey(): KeyInfo {
         const mnemonic = generateMnemonic();
         const ed25519Keypair = Ed25519Keypair.deriveKeypair(mnemonic);
         const publicKey = ed25519Keypair.getPublicKey();
@@ -13,12 +33,94 @@ export class Genkey {
             fromB64(ed25519Keypair.export().privateKey).slice(0, 32)
         )}`;
         const publicKeyStr =`0x${bytesToHex(publicKey.toBytes())}`;
-        console.log(mnemonic)
-        console.log(publicKeyStr)
-        console.log(privateKeyStr)
-        console.log(address)
+        return {
+            mnemonic: mnemonic,
+            address: address,
+            privateKey: privateKeyStr,
+            publicKey: publicKeyStr,
+        }
     }
 }
 
-Genkey.gen()
+//parse command line argruments
+const args = parse<GenKeyArguments>({
+    filepath: {type: String, alias: 'p', description: 'save file path', optional: true},
+    filename: {type: String, alias: 'f', description: 'save file name', optional: true},
+    num:  {type: Number, alias: 'n', description: 'number of generate key', optional: true},
+},
+// {
+//     helpArg: 'help',
+//     headerContentSections: [{ header: 'My Example Config', content: 'Thanks for using Our Awesome Library' }],
+//     footerContentSections: [{ header: 'Footer', content: `Copyright: Big Faceless Corp. inc.` }],
+// }
+);
+
+let { filepath, filename, num } =  {
+    filepath: args.filepath || os.homedir(),
+    filename: args.filename || "sui_key.xlsx",
+    num: args.num || 20
+}
+
+//gen key
+let keyArr: KeyInfo[] = [];
+console.log('%s %s %s %s', 'address', 'mnemonic', 'privateKey', 'publicKey');
+const genSuiKey = new GenSuikey();
+for(let idx = 0; idx < num; idx++) {
+    const keyInfo = genSuiKey.genKey();
+    console.log('%s "%s" %s %s', keyInfo.address, keyInfo.mnemonic, keyInfo.privateKey, keyInfo.publicKey)
+    keyArr.push(keyInfo);
+}
+
+let workbook = new Excel.Workbook();
+let worksheet = workbook.addWorksheet();
+worksheet.columns = [
+                    {header: "MNEMONIC", key: 'mnemonic', width:90},
+                    {header: "ADDRESS", key: 'address', width: 45},
+                    {header: "PRIVATE KEY", key: 'privateKey', width: 70},
+                    {header: "PUBLIC KEY", key: 'publicKey', width: 70},
+                ];
+// header row set bold and center
+let headerRow = worksheet.getRow(1);
+headerRow.eachCell(cell => {
+    cell.style = {
+        font: {
+            bold: true,
+        },
+        alignment:{
+            horizontal: 'center',
+        },
+    };
+});
+
+//add data
+worksheet.addRows(keyArr);
+
+//.xlsx
+if(!filename.endsWith('.xlsx')) {
+    filename += '.xlsx';
+}
+
+//write file
+let writeFilePath = path.join(filepath, filename);
+//exist file save new other file
+if(fs.existsSync(writeFilePath)) {
+    const filenameOption = path.parse(filename);    
+    filename = filenameOption.name + "_" + format(new Date(), 'yyyyMMddHHmmss') +filenameOption.ext;
+    writeFilePath = path.join(filepath, filename);
+}
+
+//write file
+console.log("key file save path: ", writeFilePath);
+writeKeyFile(workbook, writeFilePath);
+async function writeKeyFile(workbook: Excel.Workbook, writeFilePath: string){
+    await workbook.xlsx.writeFile(writeFilePath);
+}
+console.log("save key file sucessfully!");
+
+// node .\src\index.js --filepath=C:\Users\cxw\Desktop\ --filename=sui-key.xlsx --num=20
+// node .\src\index.js --p=C:\Users\cxw\Desktop\ --f=sui-key --n=20
+
+// npm run build
+// npm run start
+
 
